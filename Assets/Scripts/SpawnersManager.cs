6b5ae23aa026cd73fba47f642d09985e76b22e67
Yeenya <honza.borecky@seznam.cz>
2023-04-02 22:07:17 +0200

    Joystick pack added and fixed build error

--- Assets/Scripts/SpawnersManager.cs
+++ Assets/Scripts/SpawnersManager.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
 using UnityEditor;
 using System.Collections.Generic;
-using UnityEditor.SceneManagement;
 
 #if UNITY_EDITOR
+using UnityEditor.SceneManagement;
 public class SpawnerManager : EditorWindow
 {
     private List<Spawner> spawners = new List<Spawner>();
